801db80e93b8fe07bd9942ab7c90bbcbcaacee53
SoichiroS1066 <so11suzu@outlook.jp>
2024-11-07 12:13:25 +0900

    process_text.pyを修正

--- process_text.py
+++ process_text.py
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # 標準入力から文字を読み込む
-input_text = input("何か入力してください: ")
+input_text = input("何か文字列を入力してください: ")
 
 # 入力された文字を大文字に変換
 processed_text = input_text.upper()
 
 # 加工後の結果を標準出力に表示
-print("加工後の出力:", processed_text)
+print("変換後:", processed_text)
 
