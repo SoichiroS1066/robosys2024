55df1ba00d03a6073f79e405466f26636197f921
SoichiroS1066 <so11suzu@outlook.jp>
2024-11-07 12:11:52 +0900

    process_text.pyを作成

---
+++ process_text.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Soichiro Suzuki
+# SPDX-License-Identifier: BSD-3-Clause
+
+# 標準入力から文字を読み込む
+input_text = input("何か入力してください: ")
+
+# 入力された文字を大文字に変換
+processed_text = input_text.upper()
+
+# 加工後の結果を標準出力に表示
+print("加工後の出力:", processed_text)
+
